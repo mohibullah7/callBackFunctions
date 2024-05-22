e87df18fdaa9bc532a58641478a58a5a9658e4c6
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-05-22 15:30:16 +0500

    exercise call Back functions

---
+++ cb.js
@@ -0,0 +1,10 @@
+function getValues(a,b){
+  let input1=document.getElementById('value1').value ;
+  let input2=document.getElementById('value2').value ;
+  let value1 =Number(input1);
+let value2 =Number(input2);
+}
+
+function findPerc(){
+  
+}
