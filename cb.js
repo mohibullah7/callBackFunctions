5910ba0dec1d61b2dacba558504ec115d64247fd
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-05-22 16:01:53 +0500

    chmages haev been made

--- cb.js
+++ cb.js
@@ -1,10 +1,24 @@
+function add(c,v){
+  let addition =c +v;
+  console.log(addition);
+  }
+  
+  function findPerc(a,b){
+    let percenatge = a / b * 100;
+    console.log(percenatge);
+  
+  }
+
+
 function getValues(a,b){
   let input1=document.getElementById('value1').value ;
   let input2=document.getElementById('value2').value ;
-  let value1 =Number(input1);
-let value2 =Number(input2);
+  let value11 =Number(input1);
+  let value22 =Number(input2);
+  a(value11,value22);
+  b(value11,value22)
 }
 
-function findPerc(){
-  
+function calculate(){
+ getValues(findPerc,add)
 }
